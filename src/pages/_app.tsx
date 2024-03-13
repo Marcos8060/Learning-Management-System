8b52d77099ca43726ee2a154646a2a93b7e9ae9e
Marcos8060 <marcosgav80@gmail.com>
2024-03-13 13:52:23 +0300

    made changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+// process.env.NEXT_PUBLIC_CLIENT_ID || ''
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID || ''}>
+    <GoogleOAuthProvider clientId="169551635397-hnlptqg5umhgmmjnlv3t39g6p3i7s0sd.apps.googleusercontent.com">
       <Component {...pageProps} />
     </GoogleOAuthProvider>
   );
